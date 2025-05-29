f395319377faf51785e80fe351bbe76327cb1e71
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-29 18:51:59 +0530

    added

---
+++ q3.js
@@ -0,0 +1,20 @@
+db.products.insertMany([
+    {
+      name: "Tarun",
+      description: "Hello",
+      imageurl: "https://bsmedia.business-standard.com/_media/bs/img/article/2024-09/04/full/1725434023-926.jpg",
+      price: 1000
+    },
+    {
+      name: "Rithwick",
+      description: "Hello",
+      imageurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/004-soymilk.jpg/1200px-004-soymilk.jpg",
+      price: 2000
+    },
+    {
+      name: "Ram",
+      description: "Hello",
+      imageurl: "https://thatbreadlady.com/wp-content/uploads/2020/10/french-bread.jpg",
+      price: 3000
+    }
+  ])
